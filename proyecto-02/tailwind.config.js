de4481aac50c4373d104104172bd93d0d9903da5
wgcotera <wgcotera@espol.edu.ec>
2022-06-22 22:51:00 -0500

    build: initial setup with tailwind css and typescript

---
+++ proyecto-02/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
