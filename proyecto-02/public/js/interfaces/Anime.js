12b123a54e8a40a07cebbabb54b9ac779590f261
wgcotera <wgcotera@espol.edu.ec>
2022-07-10 23:24:04 -0500

    chore: initial bootstrap-ts setup

--- proyecto-02/public/js/interfaces/Anime.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Type;
-(function (Type) {
-    Type["Anime"] = "anime";
-    Type["Manga"] = "manga";
-})(Type || (Type = {}));
